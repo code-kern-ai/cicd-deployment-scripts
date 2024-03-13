b6894d2f46204a793a94cec16d9bbb9899e14f52
andhreljaKern <andrea.hrelja@kern.ai>
2024-03-14 00:37:10 +0100

    fix: k8 destroy by namespace

--- k8-cluster/destroy.sh
+++ k8-cluster/destroy.sh
@@ -1,6 +1,16 @@
 # !/bin/bash
 set -e
 
+KUBERNETES_NAMESPACE=""
+
+while getopts n: flag
+do
+    case "${flag}" in
+        n) KUBERNETES_NAMESPACE=${OPTARG};;
+    esac
+done
+
+kubectl config set-context --current --namespace=$KUBERNETES_NAMESPACE
 KUBERNETES_DEPLOYMENTS=$(kubectl get deployment --output json | jq -r '.items[].metadata.name')
 KUBERNETES_SERVICES=$(kubectl get service --output json | jq -r '.items[].metadata.name')
 
