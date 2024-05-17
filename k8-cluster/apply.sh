14bddc47b5a9b96978b959eb1a4849bdf067a0fe
andhreljaKern <andrea.hrelja@kern.ai>
2024-05-17 15:15:27 +0200

    perf: print kubectl apply changes only

--- k8-cluster/apply.sh
+++ k8-cluster/apply.sh
@@ -15,4 +15,4 @@ done
 kubectl config set-context --current --namespace=$KUBERNETES_NAMESPACE
 echo "Context set to namespace: \"$KUBERNETES_NAMESPACE\""
 
-kubectl apply -k $KUBERNETES_MANIFEST_PATH
+kubectl apply -k $KUBERNETES_MANIFEST_PATH | grep -E 'created|configured' || true
