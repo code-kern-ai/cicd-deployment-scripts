4472e35af4a53d440ffd41c64cd06986706ee01f
andhrelja <andhrelja@hotmail.com>
2024-05-20 12:46:36 +0200

    feat: azcopy login

--- k8-cluster/destroy.sh
+++ k8-cluster/destroy.sh
@@ -2,16 +2,32 @@
 set -e
 
 KUBERNETES_NAMESPACE=""
+AZURE_STORAGE_ACCOUNT=""
+AZURE_STORAGE_FILE_SHARE=""
 
 while getopts n: flag
 do
     case "${flag}" in
         n) KUBERNETES_NAMESPACE=${OPTARG};;
+        s) AZURE_STORAGE_ACCOUNT=${OPTARG};;
+        f) AZURE_STORAGE_FILE_SHARE=${OPTARG};;
     esac
 done
 
 kubectl config set-context --current --namespace=$KUBERNETES_NAMESPACE
 echo "Context set to namespace: \"$KUBERNETES_NAMESPACE\""
+
+azcopy login --identity
+azcopy remove \
+    "https://${AZURE_STORAGE_ACCOUNT}.file.core.windows.net/${AZURE_STORAGE_FILE_SHARE}" \
+    --exclude-path='
+        caddy/Caddyfile;
+        kratos/identity.schema.json;
+        kratos/kratos.yml;
+        oathkeeper/oathkeeeper.yml;
+        oathkeeper/access-rules.yml;
+        oathkeeper/jwks.json' \
+    --recursive=true
 
 kubectl delete namespace $KUBERNETES_NAMESPACE
 kubectl delete persistentvolumeclaims --all
