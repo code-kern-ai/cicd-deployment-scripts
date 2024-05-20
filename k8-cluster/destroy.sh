e536a83bb9e93cab1f11e4e67095876b57070c4e
andhrelja <andhrelja@hotmail.com>
2024-05-20 10:51:23 +0200

    perf: add azcopy remove commands

--- k8-cluster/destroy.sh
+++ k8-cluster/destroy.sh
@@ -13,26 +13,7 @@ done
 kubectl config set-context --current --namespace=$KUBERNETES_NAMESPACE
 echo "Context set to namespace: \"$KUBERNETES_NAMESPACE\""
 
-KUBERNETES_JOBS=$(kubectl get jobs --output json | jq -r '.items[].metadata.name')
-KUBERNETES_DEPLOYMENTS=$(kubectl get deployment --output json \
-    | jq -r '.items[] | select(.metadata.name as $name 
-        | ["caddy", "docker"] 
-        | index($name) | not) 
-    | .metadata.name')
-KUBERNETES_SERVICES=$(kubectl get service --output json \
-    | jq -r '.items[] | select(.metadata.name as $name 
-        | ["caddy", "docker"] 
-        | index($name) | not) 
-    | .metadata.name')
-
-for job in $KUBERNETES_JOBS; do
-  kubectl delete job $job
-done
-
-for deploy in $KUBERNETES_DEPLOYMENTS; do
-  kubectl delete deployment $deploy
-done
-
-for svc in $KUBERNETES_SERVICES; do
-  kubectl delete service $svc
-done
+kubectl delete namespace $KUBERNETES_NAMESPACE
+kubectl delete persistentvolumeclaims --all
+kubectl delete persistentvolumes --all
+kubectl delete storageclass --all
