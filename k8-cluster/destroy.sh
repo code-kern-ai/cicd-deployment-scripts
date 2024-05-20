2f3d93d6f64a24273767092eee37ff75ac893e60
andhrelja <andhrelja@hotmail.com>
2024-05-20 14:16:36 +0200

    fix: revert azcopy changes

--- k8-cluster/destroy.sh
+++ k8-cluster/destroy.sh
@@ -2,22 +2,16 @@
 set -e
 
 KUBERNETES_NAMESPACE=""
-AZURE_STORAGE_ACCOUNT=""
-AZURE_STORAGE_FILE_SHARE=""
 
-while getopts n:s:f: flag
+while getopts n: flag
 do
     case "${flag}" in
         n) KUBERNETES_NAMESPACE=${OPTARG};;
-        s) AZURE_STORAGE_ACCOUNT=${OPTARG};;
-        f) AZURE_STORAGE_FILE_SHARE=${OPTARG};;
     esac
 done
 
 kubectl config set-context --current --namespace=$KUBERNETES_NAMESPACE
 echo "Context set to namespace: \"$KUBERNETES_NAMESPACE\""
-
-
 
 kubectl delete namespace $KUBERNETES_NAMESPACE
 kubectl delete persistentvolumeclaims --all
