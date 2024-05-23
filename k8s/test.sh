1b5e123ed80fa64ffca324295b91511de7543393
andhrelja <andhrelja@hotmail.com>
2024-05-23 13:56:00 +0200

    fix: wait for deployment rollout instead of pod status

--- k8s/test.sh
+++ k8s/test.sh
@@ -28,7 +28,7 @@ KUBERNETES_POD_EXISTING_IMAGE=$(kubectl get pod --output json \
 kubectl set image deployment/${KUBERNETES_DEPLOYMENT_NAME} ${KUBERNETES_DEPLOYMENT_NAME}=${AZURE_CONTAINER_REGISTRY}/${KUBERNETES_DEPLOYMENT_NAME}:test-${GITHUB_HEAD_REF}
 echo "::warning::using ${AZURE_CONTAINER_REGISTRY}/${KUBERNETES_DEPLOYMENT_NAME}:test-${GITHUB_HEAD_REF}"
 
-kubectl wait --for=condition=ready pod -l app=${KUBERNETES_DEPLOYMENT_NAME}
+kubectl rollout status deployment ${KUBERNETES_DEPLOYMENT_NAME}
 
 KUBERNETES_POD_NAME=$(kubectl get pod --output json \
     --selector app=${KUBERNETES_DEPLOYMENT_NAME} \
