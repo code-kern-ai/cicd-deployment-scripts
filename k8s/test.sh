aff5508817044a0486ad0fe21ccfec5f67c18b9b
andhrelja <andhrelja@hotmail.com>
2024-05-23 13:04:10 +0200

    fix: align CI/CD scripts to Visio diagrams

--- k8s/test.sh
+++ k8s/test.sh
@@ -3,28 +3,30 @@ set -e
 
 KUBERNETES_NAMESPACE=""
 KUBERNETES_DEPLOYMENT_NAME=""
+GITHUB_HEAD_REF=""
 AZURE_CONTAINER_REGISTRY=""
 TEST_CMD=""
 
-while getopts n:d:r:t: flag
+while getopts n:d:h:r:t: flag
 do
     case "${flag}" in
         n) KUBERNETES_NAMESPACE=${OPTARG};;
         d) KUBERNETES_DEPLOYMENT_NAME=${OPTARG};;
+        h) GITHUB_HEAD_REF=${OPTARG};;
         r) AZURE_CONTAINER_REGISTRY=${OPTARG};;
         t) TEST_CMD=${OPTARG};;
     esac
 done
 
+KUBERNETES_POD_EXISTING_IMAGE=$(kubectl get pod --output json \
+    --selector app=${KUBERNETES_DEPLOYMENT_NAME} \
+    | jq -r '.items[] | .spec.containers[0].image')
 KUBERNETES_POD_NAME=$(kubectl get pod --output json \
     --selector app=${KUBERNETES_DEPLOYMENT_NAME} \
     | jq -r '.items[] | .metadata.name')
-KUBERNETES_POD_IMAGE=$(kubectl get pod --output json \
-    --selector app=${KUBERNETES_DEPLOYMENT_NAME} \
-    | jq -r '.items[] | .spec.containers[0].image')
 
-kubectl set image deployment/${KUBERNETES_DEPLOYMENT_NAME} ${KUBERNETES_DEPLOYMENT_NAME}=${AZURE_CONTAINER_REGISTRY}/${KUBERNETES_DEPLOYMENT_NAME}:test
-echo "::warning::using ${AZURE_CONTAINER_REGISTRY}/${KUBERNETES_DEPLOYMENT_NAME}:test"
+kubectl set image deployment/${KUBERNETES_DEPLOYMENT_NAME} ${KUBERNETES_DEPLOYMENT_NAME}=${AZURE_CONTAINER_REGISTRY}/${KUBERNETES_DEPLOYMENT_NAME}:test-${GITHUB_HEAD_REF}
+echo "::warning::using ${AZURE_CONTAINER_REGISTRY}/${KUBERNETES_DEPLOYMENT_NAME}:test-${GITHUB_HEAD_REF}"
 
 set +e
 exitcode=0
@@ -32,7 +34,7 @@ kubectl exec -it $KUBERNETES_POD_NAME -- $TEST_CMD
 exitcode=$?
 set -e
 
-kubectl set image deployment/${KUBERNETES_DEPLOYMENT_NAME} ${KUBERNETES_DEPLOYMENT_NAME}=${KUBERNETES_POD_IMAGE}
-echo "::notice::using ${KUBERNETES_POD_IMAGE}"
+kubectl set image deployment/${KUBERNETES_DEPLOYMENT_NAME} ${KUBERNETES_DEPLOYMENT_NAME}=${KUBERNETES_POD_EXISTING_IMAGE}
+echo "::notice::using ${KUBERNETES_POD_EXISTING_IMAGE}"
 
 exit $exitcode
