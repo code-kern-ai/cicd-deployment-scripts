f395877e8191c43d3f51d744b0fe95b8ba70c385
andhrelja <andhrelja@hotmail.com>
2024-05-26 11:37:06 +0200

    feat: add deploy success assertion

---
+++ k8s/assert_deploy_success.sh
@@ -0,0 +1,30 @@
+# !/bin/bash
+set -e
+
+KUBERNETES_NAMESPACE=""
+KUBERNETES_DEPLOYMENT_NAME=""
+APPLICATION_STARTUP_MESSAGE="Application startup complete"
+
+while getopts n:d:m: flag
+do
+    case "${flag}" in
+        n) KUBERNETES_NAMESPACE=${OPTARG};;
+        d) KUBERNETES_DEPLOYMENT_NAME=${OPTARG};;
+        m) APPLICATION_STARTUP_MESSAGE=${OPTARG};;
+    esac
+done
+
+kubectl config set-context --current --namespace=$KUBERNETES_NAMESPACE
+echo "Context set to namespace: \"$KUBERNETES_NAMESPACE\""
+
+echo "Reading logs to determine application startup status for '$KUBERNETES_DEPLOYMENT_NAME'"
+echo "Searching for message: '$APPLICATION_STARTUP_MESSAGE'"
+
+LOG_CONTENTS=$(kubectl logs deployment/${KUBERNETES_DEPLOYMENT_NAME} \
+    --container ${KUBERNETES_DEPLOYMENT_NAME} || echo "Waiting for application startuop ...")
+
+while [[ "$LOG_CONTENTS" != *"$APPLICATION_STARTUP_MESSAGE"* ]]; do
+    echo "Waiting for application startup..."
+    sleep 3
+    LOG_CONTENTS=$(kubectl logs deployment/${KUBERNETES_DEPLOYMENT_NAME} --head=100)
+done
