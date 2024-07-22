e2072161cbb81be92b9cee5fb203718edabe92a5
andhreljaKern <andrea.hrelja@kern.ai>
2024-07-22 11:30:39 +0200

    perf: use alembic rev for db migrations instead of num

--- k8s/db_rollback.sh
+++ k8s/db_rollback.sh
@@ -6,9 +6,9 @@ KUBERNETES_DEPLOYMENT_NAME=""
 KUBERNETES_NAMESPACE=""
 AZURE_CONTAINER_REGISTRY=""
 IMAGE_TAG=""
-num_migrations=""
+alembic_downgrade_rev=""
 
-while getopts e:d:r:t:n:m: flag
+while getopts e:d:r:t:n:a: flag
 do
     case "${flag}" in
         e) ENVIRONMENT_NAME=${OPTARG};;
@@ -16,34 +16,13 @@ do
         n) KUBERNETES_NAMESPACE=${OPTARG};;
         r) AZURE_CONTAINER_REGISTRY=${OPTARG};;
         t) IMAGE_TAG=${OPTARG};;
-        m) num_migrations=${OPTARG};;
+        a) alembic_downgrade_rev=${OPTARG};;
     esac
 done
 
 kubectl config set-context --current --namespace=$KUBERNETES_NAMESPACE
 echo "Context set to namespace: \"$KUBERNETES_NAMESPACE\""
 
-KUBERNETES_POD_IMAGE="${AZURE_CONTAINER_REGISTRY}/${KUBERNETES_DEPLOYMENT_NAME}:${IMAGE_TAG}"
-
-function generate_db_rollback_job {
-    echo "Generating DB Rollback job for ${KUBERNETES_DEPLOYMENT_NAME} in ${ENVIRONMENT_NAME}"
-    sed -i.bak 's|${KUBERNETES_DEPLOYMENT_NAME}|'${KUBERNETES_DEPLOYMENT_NAME}'|g' apps/${KUBERNETES_DEPLOYMENT_NAME}/${ENVIRONMENT_NAME}/db_rollback.tmpl
-    sed -i.bak 's|${KUBERNETES_POD_IMAGE}|'${KUBERNETES_POD_IMAGE}'|g' apps/${KUBERNETES_DEPLOYMENT_NAME}/${ENVIRONMENT_NAME}/db_rollback.tmpl
-    sed -i.bak 's|${num_migrations}|'${num_migrations}'|g' apps/${KUBERNETES_DEPLOYMENT_NAME}/${ENVIRONMENT_NAME}/db_rollback.tmpl
-
-    mv apps/${KUBERNETES_DEPLOYMENT_NAME}/${ENVIRONMENT_NAME}/db_rollback.tmpl apps/${KUBERNETES_DEPLOYMENT_NAME}/${ENVIRONMENT_NAME}/db_rollback.yml
-    echo "::notice::Generated DB Rollback job successfully"
-    cat apps/${KUBERNETES_DEPLOYMENT_NAME}/${ENVIRONMENT_NAME}/db_rollback.yml
-}
-
-function apply_db_rollback_job {
-    echo "Applying DB Rollback job for ${KUBERNETES_DEPLOYMENT_NAME} in ${ENVIRONMENT_NAME}"
-    kubectl apply --filename apps/${KUBERNETES_DEPLOYMENT_NAME}/${ENVIRONMENT_NAME}/db_rollback.yml
-    echo "::notice::Applied DB Rollback job successfully"
-}
-
-function delete_db_rollback_job {
-    echo "Deleting DB Rollback job for ${KUBERNETES_DEPLOYMENT_NAME} in ${ENVIRONMENT_NAME}"
-    kubectl delete --filename apps/${KUBERNETES_DEPLOYMENT_NAME}/${ENVIRONMENT_NAME}/db_rollback.yml
-    echo "::notice::Deleted DB Rollback job successfully"
-}
+echo "Rolling back to migration revision: $rev_rollback_migrations"
+kubectl exec -i deployment/${KUBERNETES_DEPLOYMENT_NAME} -c $KUBERNETES_DEPLOYMENT_NAME -- alembic downgrade ${rev_rollback_migrations}
+kubectl rollout status deployment/${KUBERNETES_DEPLOYMENT_NAME}
