cfbb85f580e60efad7a6b7b572c22647cc526c94
andhreljaKern <andrea.hrelja@kern.ai>
2024-07-23 14:50:00 +0200

    fix: syntax error in k8s/test.sh

--- k8s/test.sh
+++ k8s/test.sh
@@ -48,7 +48,7 @@ set -e
 kubectl set image deployment/${KUBERNETES_DEPLOYMENT_NAME} ${KUBERNETES_DEPLOYMENT_NAME}=${KUBERNETES_POD_EXISTING_IMAGE}
 echo "::notice::using ${KUBERNETES_POD_EXISTING_IMAGE}"
 
-if [ alembic_exitcode -eq 0 ] && [ $exitcode -ne 0 ]; then
+if [ $alembic_exitcode -eq 0 ] && [ $exitcode -ne 0 ]; then
     ALEMBIC_HEAD=${ALEMBIC_CURRENT_REVISION:0:12}
     
     ALEMBIC_UPDATED_REVISION=$(kubectl exec -i deployment/${KUBERNETES_DEPLOYMENT_NAME} -c $KUBERNETES_DEPLOYMENT_NAME -- alembic current)
@@ -60,6 +60,8 @@ if [ alembic_exitcode -eq 0 ] && [ $exitcode -ne 0 ]; then
         echo "::notice::downgrading to alembic revision: $ALEMBIC_HEAD"
         kubectl exec -i deployment/${KUBERNETES_DEPLOYMENT_NAME} -c $KUBERNETES_DEPLOYMENT_NAME -- alembic downgrade $ALEMBIC_HEAD
     fi
+elif [ $alembic_exitcode -eq 0 ] && [ $exitcode -eq 0 ]; then
+    echo "::notice::alembic upgrade completed successfully"
 fi
 
 exit $exitcode
