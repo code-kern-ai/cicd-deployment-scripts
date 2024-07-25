6ee8f7522117c88900ced4c01ea3db9a6e0384e6
andhreljaKern <andrea.hrelja@kern.ai>
2024-07-25 13:24:40 +0200

    perf: add gh-db-migration-wait workflow

---
+++ gh/db_migration_wait.sh
@@ -0,0 +1,34 @@
+# !/bin/bash
+set -e
+
+ENVIRONMENT_NAME="dev"
+K8_TEST_WORKFLOW_NAME="K8: Test"
+CURRENT_WORKFLOW_DATABASE_ID=""
+
+while getopts e:w:i: flag
+do
+    case "${flag}" in
+        e) ENVIRONMENT_NAME=${OPTARG};;
+        w) K8_TEST_WORKFLOW_NAME=${OPTARG};;
+        i) CURRENT_WORKFLOW_DATABASE_ID=${OPTARG};;
+    esac
+done
+
+
+RUNNING_DB_UPGRADE_WORKFLOW=""
+RUNNING_DB_UPGRADE_WORKFLOW_ID=$(gh run list \
+        --json conclusion,databaseId,headBranch,status,workflowName \
+        --jq '.[] | select(.workflowName=="'$K8_TEST_WORKFLOW_NAME'" and .status!="completed" and .headBranch!="'$ENVIRONMENT_NAME'" and .databaseId!="'$CURRENT_WORKFLOW_DATABASE_ID'") | .databaseId' \
+        --repo code-kern-ai/refinery-gateway)
+
+# while [ -z $RUNNING_DB_UPGRADE_WORKFLOW ]; do
+#     RUNNING_DB_UPGRADE_WORKFLOW=$(gh run list \
+#         --json conclusion,databaseId,headBranch,status,workflowName \
+#         --jq '.[] | select(.workflowName=="'$K8_TEST_WORKFLOW_NAME'" and .status!="completed" and .headBranch!="'$ENVIRONMENT_NAME'" and .databaseId!="'$CURRENT_WORKFLOW_DATABASE_ID'")' \
+#         --repo code-kern-ai/refinery-gateway)
+#     echo "Waiting for running db upgrade workflow to complete ..."
+#     if [ -z $RUNNING_DB_UPGRADE_WORKFLOW ]; then
+#         sleep 5
+#     fi
+# done
+gh run watch $RUNNING_DB_UPGRADE_WORKFLOW_ID --repo code-kern-ai/refinery-gateway
