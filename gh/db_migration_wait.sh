74c18a321e472b33040a820a55f0e61ff8f88676
andhreljaKern <andrea.hrelja@kern.ai>
2024-07-25 13:27:49 +0200

    fix: command syntax

--- gh/db_migration_wait.sh
+++ gh/db_migration_wait.sh
@@ -2,14 +2,14 @@
 set -e
 
 ENVIRONMENT_NAME="dev"
-K8_TEST_WORKFLOW_NAME="K8: Test"
+WAIT_WORKFLOW_NAME="K8: Test"
 CURRENT_WORKFLOW_DATABASE_ID=""
 
 while getopts e:w:i: flag
 do
     case "${flag}" in
         e) ENVIRONMENT_NAME=${OPTARG};;
-        w) K8_TEST_WORKFLOW_NAME=${OPTARG};;
+        w) WAIT_WORKFLOW_NAME=${OPTARG};;
         i) CURRENT_WORKFLOW_DATABASE_ID=${OPTARG};;
     esac
 done
@@ -17,14 +17,14 @@ done
 
 RUNNING_DB_UPGRADE_WORKFLOW=""
 RUNNING_DB_UPGRADE_WORKFLOW_ID=$(gh run list \
-        --json conclusion,databaseId,headBranch,status,workflowName \
-        --jq '.[] | select(.workflowName=="'$K8_TEST_WORKFLOW_NAME'" and .status!="completed" and .headBranch!="'$ENVIRONMENT_NAME'" and .databaseId!="'$CURRENT_WORKFLOW_DATABASE_ID'") | .databaseId' \
-        --repo code-kern-ai/refinery-gateway)
+    --json conclusion,databaseId,headBranch,status,workflowName \
+    --jq ".[] | select(.workflowName==\"$WAIT_WORKFLOW_NAME\" and .status!=\"completed\" and .headBranch!=\"$ENVIRONMENT_NAME\" and .databaseId!=\"$CURRENT_WORKFLOW_DATABASE_ID\") | .databaseId" \
+    --repo code-kern-ai/refinery-gateway)
 
 # while [ -z $RUNNING_DB_UPGRADE_WORKFLOW ]; do
 #     RUNNING_DB_UPGRADE_WORKFLOW=$(gh run list \
 #         --json conclusion,databaseId,headBranch,status,workflowName \
-#         --jq '.[] | select(.workflowName=="'$K8_TEST_WORKFLOW_NAME'" and .status!="completed" and .headBranch!="'$ENVIRONMENT_NAME'" and .databaseId!="'$CURRENT_WORKFLOW_DATABASE_ID'")' \
+#         --jq '.[] | select(.workflowName=="'$WAIT_WORKFLOW_NAME'" and .status!="completed" and .headBranch!="'$ENVIRONMENT_NAME'" and .databaseId!="'$CURRENT_WORKFLOW_DATABASE_ID'")' \
 #         --repo code-kern-ai/refinery-gateway)
 #     echo "Waiting for running db upgrade workflow to complete ..."
 #     if [ -z $RUNNING_DB_UPGRADE_WORKFLOW ]; then
