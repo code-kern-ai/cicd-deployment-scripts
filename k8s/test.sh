86a5e2c9090f74ea374c632576dc9b7c8fbf9158
andhreljaKern <andrea.hrelja@kern.ai>
2024-09-16 10:22:37 +0200

    perf: fail test on downgrade failure

--- k8s/test.sh
+++ k8s/test.sh
@@ -116,6 +116,9 @@ downgrade_alembic_migrations() {
             kubectl exec -i deployment/test-${REFINERY_DEPLOYMENT_NAME} -c test-${REFINERY_DEPLOYMENT_NAME} -- alembic downgrade $REFINERY_ALEMBIC_VERSION
             if [ "$?" != "0" ]; then
                 echo "::error::Alembic downgrade failed. Please update your code to support downgrading the current alembic version"
+                kubectl delete --kustomize apps/${REFINERY_DEPLOYMENT_NAME}/test
+                kubectl delete --kustomize infrastructure/test
+                exit 1
             else
                 echo "::notice::downgraded test-$REFINERY_DEPLOYMENT_NAME alembic version to $REFINERY_ALEMBIC_VERSION"
             fi
@@ -127,6 +130,9 @@ downgrade_alembic_migrations() {
         kubectl exec -i deployment/test-${KUBERNETES_DEPLOYMENT_NAME} -c test-${KUBERNETES_DEPLOYMENT_NAME} -- alembic downgrade $KUBERNETES_DEPLOYMENT_ALEMBIC_VERSION
         if [ "$?" != "0" ]; then
             echo "::error::Alembic downgrade failed. Please update your code to support downgrading the current alembic version"
+            kubectl delete --kustomize apps/${REFINERY_DEPLOYMENT_NAME}/test
+            kubectl delete --kustomize infrastructure/test
+            exit 1
         else
             echo "::notice::downgraded test-$KUBERNETES_DEPLOYMENT_NAME alembic version to $REFINERY_ALEMBIC_VERSION"
         fi
