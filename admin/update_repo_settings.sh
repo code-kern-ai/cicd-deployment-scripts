3c51b5703ce9af4b6a95acc46462043a84083ebc
andhreljaKern <andrea.hrelja@kern.ai>
2024-10-07 10:40:13 +0200

    perf: make general settings a static file

--- admin/update_repo_settings.sh
+++ admin/update_repo_settings.sh
@@ -24,7 +24,7 @@ function create_ruleset() {
     REPOSITORY_NAME=${2}
     ENVIRONMENT_NAME=${3}
 
-    echo "Creating ruleset for ${REPOSITORY_NAME} - ${ENVIRONMENT_NAME}"
+    echo "Creating ruleset for ${ENVIRONMENT_NAME} - ${REPOSITORY_OWNER}/${REPOSITORY_NAME}"
 
     RULESET_CONTENT=$(echo $(sed \
         -e "s|\${ENVIRONMENT_NAME}|${ENVIRONMENT_NAME}|g" \
@@ -46,7 +46,7 @@ function update_ruleset() {
     ENVIRONMENT_NAME=${3}
     RULESET_ID=${4}
 
-    echo "Updating ruleset for ${REPOSITORY_NAME} - ${ENVIRONMENT_NAME}"
+    echo "Updating ruleset for ${ENVIRONMENT_NAME} - ${REPOSITORY_OWNER}/${REPOSITORY_NAME}"
 
     RULESET_CONTENT=$(echo $(sed \
         -e "s|\${ENVIRONMENT_NAME}|${ENVIRONMENT_NAME}|g" \
@@ -65,39 +65,24 @@ function update_ruleset() {
 function update_repo_general_settings() {
     REPOSITORY_OWNER=${1}
 
-    echo "::group::Updating repository settings"
+    GENERAL_CONTENT=$(cat admin/repo_static/general.json)
     COMBINED_ARRAY=(${REPO_LIST_APP_IAC[@]} ${REPO_LIST_TF_IAC[@]} ${REPO_LIST_TF_MODULE[@]})
     for REPOSITORY_NAME in ${COMBINED_ARRAY[@]}; do
-        echo "Updating ${REPOSITORY_OWNER}/${REPOSITORY_NAME}"
-        gh api \
+        echo "Updating general settings for ${REPOSITORY_OWNER}/${REPOSITORY_NAME}"
+        echo "${GENERAL_CONTENT}" | gh api \
             --method PATCH \
             -H "Accept: application/vnd.github+json" \
             -H "X-GitHub-Api-Version: 2022-11-28" \
             /repos/${REPOSITORY_OWNER}/${REPOSITORY_NAME} \
-            -F "has_issues=true" \
-            -F "has_projects=false" \
-            -F "has_wiki=false" \
-            -F "allow_squash_merge=true" \
-            -F "allow_merge_commit=true" \
-            -F "allow_rebase_merge=false" \
-            -F "allow_auto_merge=false" \
-            -F "delete_branch_on_merge=true" \
-            -F "allow_update_branch=true" 1>/dev/null
+            --input - 1>/dev/null
     done
-    echo "::endgroup::"
 }
 
 function update_tf_module_rulesets() {
     REPOSITORY_OWNER=${1}
     ENVIRONMENT_NAME=${2}
 
-    echo "::group::tf-module repository rulesets"
     for REPOSITORY_NAME in ${REPO_LIST_TF_MODULE[@]}; do
-        if [ "${ENVIRONMENT_NAME}" = "prod" ]; then
-            # Module repositories do not need a prod ruleset
-            continue
-        fi
-
         ruleset_id=$(get_ruleset_by_name ${REPOSITORY_OWNER} ${REPOSITORY_NAME} ${ENVIRONMENT_NAME})
         if [ -z "${ruleset_id}" ]; then
             create_ruleset ${REPOSITORY_OWNER} ${REPOSITORY_NAME} ${ENVIRONMENT_NAME}
@@ -105,14 +90,12 @@ function update_tf_module_rulesets() {
             update_ruleset ${REPOSITORY_OWNER} ${REPOSITORY_NAME} ${ENVIRONMENT_NAME} ${ruleset_id}
         fi
     done
-    echo "::endgroup::"
 }
 
 function update_tf_iac_rulesets() {
     REPOSITORY_OWNER=${1}
     ENVIRONMENT_NAME=${2}
 
-    echo "::group::app-tf-iac repository rulesets"
     COMBINED_ARRAY=(${REPO_LIST_APP_IAC[@]} ${REPO_LIST_TF_IAC[@]})
     for REPOSITORY_NAME in ${COMBINED_ARRAY[@]}; do
         ruleset_id=$(get_ruleset_by_name ${REPOSITORY_OWNER} ${REPOSITORY_NAME} ${ENVIRONMENT_NAME})
@@ -122,5 +105,4 @@ function update_tf_iac_rulesets() {
             update_ruleset ${REPOSITORY_OWNER} ${REPOSITORY_NAME} ${ENVIRONMENT_NAME} ${ruleset_id}
         fi
     done
-    echo "::endgroup::"
 }
