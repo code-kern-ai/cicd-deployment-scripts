e2248ae9ba8cb3ed5c90edb920d69d28380486cb
andhreljaKern <andrea.hrelja@kern.ai>
2024-10-08 09:57:59 +0200

    fix: function definition ordering

--- k8s/test.sh
+++ k8s/test.sh
@@ -25,6 +25,45 @@ do
     esac
 done
 
+
+REFINERY_ALEMBIC_VERSION=""
+KUBERNETES_DEPLOYMENT_ALEMBIC_VERSION=""
+
+REFINERY_IMAGE_TAG_EXISTS=$(az acr repository show --name ${AZURE_CONTAINER_REGISTRY} --image ${REFINERY_DEPLOYMENT_NAME}:${TEST_IMAGE_TAG} 2> /dev/null || true)
+
+downgrade_alembic_migrations() {
+    echo "::group::Downgrade alembic migrations"
+    if [ $KUBERNETES_DEPLOYMENT_NAME != "refinery-gateway" ] && [ $KUBERNETES_DEPLOYMENT_NAME != "gates-gateway" ] && [ $KUBERNETES_DEPLOYMENT_NAME != "hosted-inference-api" ]; then
+        if [ -n "$REFINERY_IMAGE_TAG_EXISTS" ]; then
+            set +e
+            kubectl exec -i deployment/test-${REFINERY_DEPLOYMENT_NAME} -c test-${REFINERY_DEPLOYMENT_NAME} -- alembic downgrade $REFINERY_ALEMBIC_VERSION
+            if [ "$?" != "0" ]; then
+                echo "::error::Alembic downgrade failed. Please update your code to support downgrading the current alembic version"
+                kubectl delete --kustomize apps/${REFINERY_DEPLOYMENT_NAME}/test
+                kubectl delete --kustomize infrastructure/test
+                exit 1
+            else
+                echo "::notice::downgraded test-$REFINERY_DEPLOYMENT_NAME alembic version to $REFINERY_ALEMBIC_VERSION"
+            fi
+            set -e
+        fi
+        kubectl delete --kustomize apps/${REFINERY_DEPLOYMENT_NAME}/test
+    else
+        set +e
+        kubectl exec -i deployment/test-${KUBERNETES_DEPLOYMENT_NAME} -c test-${KUBERNETES_DEPLOYMENT_NAME} -- alembic downgrade $KUBERNETES_DEPLOYMENT_ALEMBIC_VERSION
+        if [ "$?" != "0" ]; then
+            echo "::error::Alembic downgrade failed. Please update your code to support downgrading the current alembic version"
+            kubectl delete --kustomize apps/${REFINERY_DEPLOYMENT_NAME}/test
+            kubectl delete --kustomize infrastructure/test
+            exit 1
+        else
+            echo "::notice::downgraded test-$KUBERNETES_DEPLOYMENT_NAME alembic version to $REFINERY_ALEMBIC_VERSION"
+        fi
+        set -e
+    fi
+    echo "::endgroup::"
+}
+
 __safe_migration_rollout() {
     deploy=$1
 
@@ -47,24 +86,6 @@ __safe_migration_rollout() {
         kill -s TERM $TOP_PID
     fi
 }
-
-echo "::group::Kubernetes Context & Test Infrastructure"
-kubectl config set-context --current --namespace=$KUBERNETES_NAMESPACE
-echo "Context set to namespace: \"$KUBERNETES_NAMESPACE\""
-kubectl apply --kustomize infrastructure/test
-__safe_migration_rollout test-postgres
-until kubectl exec -i deployment/test-postgres -- sh -c pg_isready; do
-    echo "Waiting for postgres to be ready..."
-    sleep 3
-done
-kubectl exec -i deployment/test-postgres -- sh -c "psql -U postgres -c '$(cat infrastructure/test/deployment/assets/init.sql)'"
-echo "::endgroup::"
-
-
-REFINERY_ALEMBIC_VERSION=""
-KUBERNETES_DEPLOYMENT_ALEMBIC_VERSION=""
-
-REFINERY_IMAGE_TAG_EXISTS=$(az acr repository show --name ${AZURE_CONTAINER_REGISTRY} --image ${REFINERY_DEPLOYMENT_NAME}:${TEST_IMAGE_TAG} 2> /dev/null || true)
 
 upgrade_alembic_migrations() {
     echo "::group::Upgrade alembic migrations for test"
@@ -108,38 +129,18 @@ upgrade_alembic_migrations() {
     echo "::endgroup::"
 }
 
-downgrade_alembic_migrations() {
-    echo "::group::Downgrade alembic migrations"
-    if [ $KUBERNETES_DEPLOYMENT_NAME != "refinery-gateway" ] && [ $KUBERNETES_DEPLOYMENT_NAME != "gates-gateway" ] && [ $KUBERNETES_DEPLOYMENT_NAME != "hosted-inference-api" ]; then
-        if [ -n "$REFINERY_IMAGE_TAG_EXISTS" ]; then
-            set +e
-            kubectl exec -i deployment/test-${REFINERY_DEPLOYMENT_NAME} -c test-${REFINERY_DEPLOYMENT_NAME} -- alembic downgrade $REFINERY_ALEMBIC_VERSION
-            if [ "$?" != "0" ]; then
-                echo "::error::Alembic downgrade failed. Please update your code to support downgrading the current alembic version"
-                kubectl delete --kustomize apps/${REFINERY_DEPLOYMENT_NAME}/test
-                kubectl delete --kustomize infrastructure/test
-                exit 1
-            else
-                echo "::notice::downgraded test-$REFINERY_DEPLOYMENT_NAME alembic version to $REFINERY_ALEMBIC_VERSION"
-            fi
-            set -e
-        fi
-        kubectl delete --kustomize apps/${REFINERY_DEPLOYMENT_NAME}/test
-    else
-        set +e
-        kubectl exec -i deployment/test-${KUBERNETES_DEPLOYMENT_NAME} -c test-${KUBERNETES_DEPLOYMENT_NAME} -- alembic downgrade $KUBERNETES_DEPLOYMENT_ALEMBIC_VERSION
-        if [ "$?" != "0" ]; then
-            echo "::error::Alembic downgrade failed. Please update your code to support downgrading the current alembic version"
-            kubectl delete --kustomize apps/${REFINERY_DEPLOYMENT_NAME}/test
-            kubectl delete --kustomize infrastructure/test
-            exit 1
-        else
-            echo "::notice::downgraded test-$KUBERNETES_DEPLOYMENT_NAME alembic version to $REFINERY_ALEMBIC_VERSION"
-        fi
-        set -e
-    fi
-    echo "::endgroup::"
-}
+echo "::group::Kubernetes Context & Test Infrastructure"
+kubectl config set-context --current --namespace=$KUBERNETES_NAMESPACE
+echo "Context set to namespace: \"$KUBERNETES_NAMESPACE\""
+kubectl apply --kustomize infrastructure/test
+__safe_migration_rollout test-postgres
+until kubectl exec -i deployment/test-postgres -- sh -c pg_isready; do
+    echo "Waiting for postgres to be ready..."
+    sleep 3
+done
+kubectl exec -i deployment/test-postgres -- sh -c "psql -U postgres -c '$(cat infrastructure/test/deployment/assets/init.sql)'"
+echo "::endgroup::"
+
 
 if [ "$ENABLE_ALEMBIC_MIGRATIONS" = "true" ]; then
     upgrade_alembic_migrations
