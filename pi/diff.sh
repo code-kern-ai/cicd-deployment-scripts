72063ac0e2a9763425c957a36f49d279ac42d85b
andhreljaKern <andrea.hrelja@kern.ai>
2024-12-05 16:44:53 +0100

    fix: outputting matrix

--- pi/diff.sh
+++ pi/diff.sh
@@ -27,17 +27,14 @@ while IFS= read -r file; do
 
 done <<< "$UPDATED_FILES"
 
-PARENT_IMAGE_TYPES=""
 INCLUDES=""
 for parent_image_type in "${UPDATED_PARENT_TYPES[@]}"; do
-    PARENT_IMAGE_TYPES+='"'$parent_image_type'",'
     eval 'APP_REPOS=( "${'$(echo ${parent_image_type} | sed "s|-|_|g")'[@]}" )'
     for app in "${APP_REPOS[@]}"; do
         INCLUDES+='{ "parent_image_type": "'${parent_image_type}'", "app": "'${app}'" },'
     done
 done
 
-MATRIX='{"parent_image_type": ['${PARENT_IMAGE_TYPES::-1}'],"include": ['${INCLUDES::-1}']}'
+MATRIX='{"include": ['${INCLUDES::-1}']}'
 echo $MATRIX | jq -C --indent 2 '.'
-echo "parent_image_type=[${PARENT_IMAGE_TYPES::-1}]" >> $GITHUB_OUTPUT
 echo "include=[${INCLUDES::-1}]" >> $GITHUB_OUTPUT
