d686ad3c49230b4dea24f110e21092a2a7c626d4
andhreljaKern <andrea.hrelja@kern.ai>
2024-12-09 13:24:27 +0100

    perf: add next support perf: support mutually dependent parent image types

--- pi/settings.sh
+++ pi/settings.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-export PARENT_IMAGE_TYPES=(
-    mini
-    common
-    exec_env
-    torch_cpu
-    torch_cuda
-    next
-)
-
 export mini=(
     "refinery-authorizer"
-    # "refinery-config"
-    # "refinery-doc-ock"
     "refinery-gateway-proxy"
-    # "platform-monitoring"
 )
 
 export common=(
@@ -24,39 +12,31 @@ export common=(
     "refinery-updater"
     "refinery-weak-supervisor"
     "refinery-model-provider"
-    # "refinery-commercial-proxy"
-    # "gates-gateway"
-    # "chat-gateway"
     "cognition-gateway"
+    "${mini[@]}"
 )
 
 export exec_env=(
     "refinery-ac-exec-env"
     "refinery-lf-exec-env"
-    # "refinery-record-ide-env"
-    # "gates-runtime"
-    # "chat-exec-env"
     "cognition-exec-env"
 )
 
 export torch_cpu=(
     "refinery-embedder"
     "refinery-ml-exec-env"
-    # "refinery-zero-shot"
-    # "hosted-inference-api"
+    "${common[@]}"
 )
 
-export torch_gpu=(
+export torch_cuda=(
     "refinery-embedder"
-    # "refinery-zero-shot"
+    "${common[@]}"
 )
 
 export next=(
-    # "gates-ui"
     "admin-dashboard"
-    # "chat-ui"
     "cognition-ui"
     "refinery-ui"
 )
 
-export ALL_SERVICES=( "${mini[@]}" "${common[@]}" "${exec_env[@]}" "${torch_cpu[@]}" "${torch_gpu[@]}" )
+export ALL_SERVICES=( "${mini[@]}" "${common[@]}" "${exec_env[@]}" "${torch_cpu[@]}" "${torch_cuda[@]}" )
