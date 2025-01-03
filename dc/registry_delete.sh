91cd0803804c7623d151fd410ca07d4de872e79d
andhreljaKern <andrea.hrelja@kern.ai>
2025-01-03 11:11:50 +0100

    perf: add validate_image_tag function

--- dc/registry_delete.sh
+++ dc/registry_delete.sh
@@ -5,8 +5,8 @@ ENVIRONMENT_NAME="dev"
 GITHUB_OWNER="code-kern-ai"
 REGISTRY_URL="registry.dev.kern.ai"
 APP_NAME="hosted-inference-api"
-DELETE_TAG="andhrelja"
-HTTPS_USERNAME="andrea:Jasan91007"
+DELETE_TAG=""
+HTTPS_USERNAME=""
 DELETE_SINCE_DAYS=""
 
 while getopts e:g:r:a:t:u:d: flag
@@ -23,24 +23,34 @@ do
 done
 
 
+function validate_image_tag() {
+    manifest=$1
+    errors=$(echo $manifest | jq -r '.errors')
+    if [ $errors != null ]; then
+        echo "::error::$REGISTRY_URL/$APP_NAME:$DELETE_TAG => $(echo $errors | jq -r '.[0].code')"
+        echo $manifest | jq -rc '.errors'
+        exit 1
+    fi
+}
+
 # Delete images older than DELETE_SINCE_DAYS and exit 0
 if [ -n "$DELETE_SINCE_DAYS" ]; then
     echo "::notice::Deleting images older than $DELETE_SINCE_DAYS days"
     repo_tags=$(curl -s -u $HTTPS_USERNAME https://$REGISTRY_URL/v2/$GITHUB_OWNER/$APP_NAME/tags/list | jq -r '.tags[]')
     while IFS= read -r tag; do
-        created=$(curl -s -u $HTTPS_USERNAME \
-            https://$REGISTRY_URL/v2/$GITHUB_OWNER/$APP_NAME/manifests/$tag \
-            | jq -r '.history[0].v1Compatibility' | jq -r '.created')
+        manifest=$(curl -s -u $HTTPS_USERNAME \
+            -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+            https://$REGISTRY_URL/v2/$GITHUB_OWNER/$APP_NAME/manifests/$DELETE_TAG)
         
+        validate_image_tag $manifest
+
+        created=$(echo $manifest | jq -r '.history[0].v1Compatibility' | jq -r '.created')
         created_date=$(date -d $created +%s)
         current_date=$(date +%s)
         days_since=$(( (current_date - created_date) / (60*60*24) ))
         
         if [ $days_since -gt $DELETE_SINCE_DAYS ]; then
-            digest=$(curl -s -u $HTTPS_USERNAME \
-                -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
-                https://$REGISTRY_URL/v2/$GITHUB_OWNER/$APP_NAME/manifests/$tag \
-                | sha256sum | cut -d ' ' -f 1)
+            digest=$(echo $manifest | sha256sum | cut -d ' ' -f 1)
             curl -X DELETE -u $HTTPS_USERNAME https://$REGISTRY_URL/v2/$GITHUB_OWNER/$APP_NAME/manifests/sha256:$digest
             echo "::warning::deleted $APP_NAME:$tag, $days_since days old"
         else
@@ -56,12 +66,7 @@ if [ -n $DELETE_TAG ]; then
         -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
         https://$REGISTRY_URL/v2/$GITHUB_OWNER/$APP_NAME/manifests/$DELETE_TAG)
     
-    errors=$(echo $manifest | jq -r '.errors')
-    if [ $errors != null ]; then
-        echo "::error::$REGISTRY_URL/$APP_NAME:$DELETE_TAG => $(echo $errors | jq -r '.[0].code')"
-        echo $manifest | jq -rc '.errors'
-        exit 1
-    fi
+    validate_image_tag $manifest
     
     digest=$(echo $manifest | sha256sum | cut -d ' ' -f 1)
     
