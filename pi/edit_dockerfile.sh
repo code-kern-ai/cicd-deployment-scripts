0d25ab2926f54aff2c50e3cb850c0631b4d97afb
andhreljaKern <andrea.hrelja@kern.ai>
2025-07-22 11:36:45 +0200

    feat: parent image improvements

--- pi/edit_dockerfile.sh
+++ pi/edit_dockerfile.sh
@@ -2,14 +2,15 @@
 
 set -e
 
+HEAD_REF="parent-image-updates"
 PARENT_IMAGE_NAME="refinery-parent-images"
-RELEASE_TAG="v2.0.0"
+RELEASE_TAG="latest"
 DOCKER_REGISTRY="kernai"
 DEV_REGISTRY="registry.dev.kern.ai/code-kern-ai"
 DOCKERFILE="Dockerfile"
-HEAD_REF=""
+EDIT_TYPE="build"
 
-while getopts i:l:r:e:d:h: flag
+while getopts i:l:r:e:d:t: flag
 do
     case "${flag}" in
         i) PARENT_IMAGE_NAME=${OPTARG};;
@@ -17,40 +18,43 @@ do
         r) DOCKER_REGISTRY=${OPTARG};;
         e) DEV_REGISTRY=${OPTARG};;
         d) DOCKERFILE=${OPTARG};;
-        h) HEAD_REF=${OPTARG};;
+        t) EDIT_TYPE=${OPTARG};;
     esac
 done
 
-if [ -z "$HEAD_REF" ]; then
-    REGISTRY=$DOCKER_REGISTRY
+DEV_UPDATED=$(grep "${DEV_REGISTRY}/${PARENT_IMAGE_NAME}:${HEAD_REF}" $DOCKERFILE)
+if [ "$EDIT_TYPE" = "build" ]; then
+    if [ -n "$DEV_UPDATED" ]; then
+        echo "Parent image already updated in $DOCKERFILE"
+        exit 0
+    else
+        REGISTRY="${DOCKER_REGISTRY}"
+    fi
+elif [ "$EDIT_TYPE" = "release" ]; then
+    if [ -n "$DEV_UPDATED" ]; then
+        REGISTRY="${DEV_REGISTRY}"
+    else
+        REGISTRY="${DOCKER_REGISTRY}"
+    fi
 else
-    REGISTRY=$DEV_REGISTRY
+    echo "::error::Invalid EDIT_TYPE: $EDIT_TYPE. Use 'build' or 'release'"
+    exit 1
 fi
 
 grep "${REGISTRY}/${PARENT_IMAGE_NAME}" $DOCKERFILE | while read -r line ; do
     PI_EXISTING_TAG=$(echo $line | sed 's|FROM ||g' | cut -d ':' -f 2)
-    ALREADY_UPDATED=$(echo $PI_EXISTING_TAG | grep $RELEASE_TAG || true)
+    PI_EXISTING_IMAGE="${REGISTRY}/${PARENT_IMAGE_NAME}:${PI_EXISTING_TAG}"
 
-    if [ -z "$ALREADY_UPDATED" ] && [ -z "$HEAD_REF" ]; then
-        image_version=$(echo $PI_EXISTING_TAG | cut -d '-' -f 1)
-        PARENT_IMAGE_TYPE=$(echo $PI_EXISTING_TAG | sed "s|${image_version}-||g")
-    elif [ -n "$ALREADY_UPDATED" ] && [ -z "$HEAD_REF" ]; then
-        PARENT_IMAGE_TYPE=$(echo $PI_EXISTING_TAG | sed "s|${RELEASE_TAG}-||g")
-    elif [ -z "$ALREADY_UPDATED" ] && [ -n "$HEAD_REF" ]; then
-        image_version=$(echo $PI_EXISTING_TAG | cut -d '-' -f 1)
-        PARENT_IMAGE_TYPE=$(echo $PI_EXISTING_TAG | sed "s|${image_version}-||g")
-    elif [ -n "$ALREADY_UPDATED" ] && [ -n "$HEAD_REF" ]; then
+    if [ -n "$DEV_UPDATED" ]; then
         PARENT_IMAGE_TYPE=$(echo $PI_EXISTING_TAG | sed "s|${HEAD_REF}-||g")
     else
-        echo "::error::Failed to determine parent image type from tag: ${PI_EXISTING_TAG}"
-        exit 1
+        image_version=$(echo $PI_EXISTING_TAG | cut -d '-' -f 1)
+        PARENT_IMAGE_TYPE=$(echo $PI_EXISTING_TAG | sed "s|${image_version}-||g")
     fi
 
-    if [ -z "$HEAD_REF" ]; then
-        PI_EXISTING_IMAGE="${DOCKER_REGISTRY}/${PARENT_IMAGE_NAME}:${PI_EXISTING_TAG}"
-        PI_NEW_IMAGE="${DEV_REGISTRY}/${PARENT_IMAGE_NAME}:${RELEASE_TAG}-${PARENT_IMAGE_TYPE}"
-    else
-        PI_EXISTING_IMAGE="${DEV_REGISTRY}/${PARENT_IMAGE_NAME}:${PI_EXISTING_TAG}"
+    if [ "$EDIT_TYPE" = "build" ]; then
+        PI_NEW_IMAGE="${DEV_REGISTRY}/${PARENT_IMAGE_NAME}:${HEAD_REF}-${PARENT_IMAGE_TYPE}"
+    elif [ "$EDIT_TYPE" = "release" ]; then
         PI_NEW_IMAGE="${DOCKER_REGISTRY}/${PARENT_IMAGE_NAME}:${RELEASE_TAG}-${PARENT_IMAGE_TYPE}"
     fi
 
